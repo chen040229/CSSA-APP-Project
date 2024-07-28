f8cfa5cfea1c658cc93312ba91c1eebbe9c92363
chen040229 <145748474+chen040229@users.noreply.github.com>
2024-07-27 20:32:52 -0700

    Add files via upload

---
+++ Week1/Person-components/Personpic.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import doggy from './doggy.jpg';
+
+function Personpic() {
+  return (
+    <img src = {doggy} alt = 'Picture' className="personal-image"/>
+  )
+}
+
+export default Personpic
